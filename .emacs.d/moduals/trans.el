c0f79161dee2037df6bb2701aacdd57966796b1b
Ayako <ayako.gfx@gmail.com>
2025-03-02 02:57:39 +0530

    Update trans.el

--- .emacs.d/moduals/trans.el
+++ .emacs.d/moduals/trans.el
@@ -1,19 +1,17 @@
-;; Set transparency for all frames
+;; Set transparency for all frames (active and inactive)
 (add-to-list 'default-frame-alist '(alpha . (90 . 90)))
 
 ;; Optional: Set transparency for the current frame
 (set-frame-parameter (selected-frame) 'alpha '(90 . 90))
 
+;; Define a function to toggle transparency
 (defun toggle-transparency ()
   "Toggle transparency of the current frame."
   (interactive)
   (let ((alpha (frame-parameter nil 'alpha)))
-    (if (eql (cond ((numberp alpha) alpha)
-                   ((numberp (cdr alpha)) (cdr alpha))
-                   ;; Also handle inactive alpha.
-                   ((numberp (cadr alpha)) (cadr alpha)))
-        100)
-        (set-frame-parameter nil 'alpha '(90 . 90)) ; Set to 90% opacity
-      (set-frame-parameter nil 'alpha '(100 . 100))))) ; Set to 100% opacity
+    (if (equal alpha '(90 . 90)) ; Check if current transparency is 90%
+        (set-frame-parameter nil 'alpha '(100 . 100)) ; Set to 100% opacity
+      (set-frame-parameter nil 'alpha '(90 . 90))))) ; Set to 90% opacity
 
+;; Bind the function to F5
 (global-set-key (kbd "<f5>") 'toggle-transparency)
