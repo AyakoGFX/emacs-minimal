72b527182c17299782121a57db888c93ae811b17
Ayako <ayako.gfx@gmail.com>
2025-03-01 22:28:36 +0530

    Update shell.el

--- .emacs.d/moduals/shell.el
+++ .emacs.d/moduals/shell.el
@@ -1,4 +1,11 @@
-(setq explicit-shell-file-name "/run/current-system/sw/bin/bash")
-(setq explicit-bash-args '("--login" "-i"))
-(setq term-shell "/run/current-system/sw/bin/bash")
-(setq shell-file-name "/run/current-system/sw/bin/bash")
+(if (string= (shell-command-to-string "grep -q NixOS /etc/os-release && echo yes || echo no") "yes\n")
+    (progn
+      (setq explicit-shell-file-name "/run/current-system/sw/bin/bash")
+      (setq explicit-bash-args '("--login" "-i"))
+      (setq term-shell "/run/current-system/sw/bin/bash")
+      (setq shell-file-name "/run/current-system/sw/bin/bash"))
+  (progn
+    (setq explicit-shell-file-name "/usr/bin/bash")
+    (setq explicit-bash-args '("--login" "-i"))
+    (setq term-shell "/usr/bin/bash")
+    (setq shell-file-name "/usr/bin/bash")))
