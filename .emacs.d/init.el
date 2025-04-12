eb395de8674d36275cf2174aaa01a0ecc0b78b27
Ayako <ayako.gfx@gmail.com>
2025-04-12 15:20:22 +0530

    z

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -85,6 +85,23 @@
 (load "~/.emacs.d/moduals/git.el")
 (load "~/.emacs.d/moduals/project.el")
 
+
+
+(defun play-sound-with-paplay (file)
+  "Play a sound file using paplay."
+  (let ((sound-file (expand-file-name file "~/.emacs.d/sounds/")))
+    (if (file-exists-p sound-file)
+        (start-process "sound-player" nil "paplay" sound-file)
+      (message "Sound file not found: %s" sound-file))))
+
+(advice-add 'kill-region :after (lambda (&rest _) (play-sound-with-paplay "CUT.wav")))
+(advice-add 'kill-ring-save :after (lambda (&rest _) (play-sound-with-paplay "COPY.wav")))
+(advice-add 'yank :after (lambda (&rest _) (play-sound-with-paplay "PAST.wav")))
+
+;; (play-sound-with-paplay "CUT.wav")
+
+
+
 ;; Emacs: display ugly ^L page breaks as tidy horizontal lines
 ;; Navigate between them: Press C-x [ and C-x ] to move between page breaks.
 ;; Delete them: Use M-% ^L RET RET to replace all occurrences with nothing.
